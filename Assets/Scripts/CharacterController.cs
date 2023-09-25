d50513afc4a6f9c5a4e2f3f37656428438553e20
William <50932749+BilliamsFluster@users.noreply.github.com>
2023-09-24 22:48:07 -0400

    Annotated with comments

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -4,40 +4,72 @@ using UnityEngine;
 
 public class CharacterController : Controller
 {
+    // Boolean to determine if the character should rotate based on mouse position
     public bool isMouseRotation;
-    // Start is called before the first frame update
+
+    // Initialization function called once at the start (currently empty)
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
+    // Function called every frame
     void Update()
     {
+        // Get the movement direction based on player input (Horizontal and Vertical axes)
         Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        
+
+        // Move the pawn (character) in the specified direction
         pawn.Move(moveDirection);
-        if(isMouseRotation)
+
+        // If mouse-based rotation is enabled
+        if (isMouseRotation)
         {
             Vector3 pointToLookAt;
+            // Create a plane at the character's position, facing upwards
             Plane plane = new Plane(Vector3.up, pawn.transform.position);
+
+            // Create a ray from the camera through the mouse position
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
+            
+
+
             float distance;
+            // Check if the ray intersects with the plane
             plane.Raycast(ray, out distance);
+
+            // Get the intersection point on the plane
             pointToLookAt = ray.GetPoint(distance);
+
+            // Rotate the pawn to face the intersection point
             pawn.RotateToLookAt(pointToLookAt);
         }
-        
+
+        // If mouse-based rotation is not enabled
         else
+        {
+            // Rotate the pawn based on the "Rotation" axis input
             pawn.Rotate(Input.GetAxis("Rotation"));
+        }
+        if (pawn != null)
+        {
+            Vector3 cameraPosition = Camera.main.transform.position;
+            cameraPosition.x = pawn.transform.position.x;
+            cameraPosition.z = pawn.transform.position.z;
+            Camera.main.transform.position = cameraPosition;
+
+        }
+
     }
 
+    // Function to possess a pawn (currently empty)
     public override void Possess(Pawn pawn)
     {
 
     }
 
+    // Function to unpossess a pawn (currently empty)
     public override void UnPossess(Pawn pawn)
     {
 
--- Assets/Scripts/Pawn.cs
+++ Assets/Scripts/Pawn.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using UnityEngine;
 
+// Define an abstract class named Pawn, which inherits from MonoBehaviour
 public abstract class Pawn : MonoBehaviour
 {
+    // Reference to the Controller that controls this Pawn
     public Controller controller;
+
+    // Movement speed of the Pawn
     public float moveSpeed;
+
+    // Rotation speed of the Pawn
     public float rotationSpeed;
 
+    // Abstract method to move the Pawn in a specified direction
+    // This method must be implemented by any non-abstract class that inherits from Pawn
     public abstract void Move(Vector3 direction);
-    public abstract void Rotate(float direction);
-    public abstract void RotateToLookAt(Vector3 position);
-    
 
+    // Abstract method to rotate the Pawn based on a specified direction
+    // This method must be implemented by any non-abstract class that inherits from Pawn
+    public abstract void Rotate(float direction);
+
+    // Abstract method to rotate the Pawn to face a specified position
+    // This method must be implemented by any non-abstract class that inherits from Pawn
+    public abstract void RotateToLookAt(Vector3 position);
 }
